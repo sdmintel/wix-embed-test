f0c481594fc79a5005dfb4b0ff5e15c7eb908d45
sdmintel <shanedmfl@gmail.com>
2025-05-02 10:09:25 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+const FUNDERS_DATA = [
+    { name: "Tech Ventures", industry: ["tech"], minInvestment: 100000, maxInvestment: 1000000 },
+    { name: "Health Capital", industry: ["health"], minInvestment: 500000, maxInvestment: 2000000 },
+    { name: "Finance Fund", industry: ["finance"], minInvestment: 250000, maxInvestment: 1500000 }
+];
